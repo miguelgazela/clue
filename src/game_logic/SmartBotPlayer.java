d029c569956a99c37abb3c1554fd128a03c4e9f0
Acaldas <afonso7caldas@gmail.com>
2013-12-13 13:09:17 +0000

    disktra goes around blocked tiles. Changes on smartBotPlayer

--- src/game_logic/SmartBotPlayer.java
+++ src/game_logic/SmartBotPlayer.java
@@ -1,11 +1,18 @@
 package game_logic;
 
+import java.util.ArrayList;
+
 import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 
 public class SmartBotPlayer extends BotPlayerAgent {
 
 	private static final long serialVersionUID = 7479738606280331848L;
+	private String roomSolutionString = null;
+	private String suspectSolutionString = null;
+	private String weaponSolutionString = null;
+	private ArrayList<Tile> minPath;
+	private Coordinates targetCoord = null;
 	
 	public void setup() {
 		super.setup();
@@ -14,7 +21,38 @@ public class SmartBotPlayer extends BotPlayerAgent {
 
 	@Override
 	public void makePlay(ACLMessage msg) {
-		// TODO Auto-generated method stub
+		Tile currentTile = gameState.board.getTileAtPosition(posOnBoard);
+		
+		// Room is known.
+		if (roomSolutionString != null) {
+			if (currentTile.isRoom()) {
+				// In solution room.
+				if (currentTile.getRoom().equals(roomSolutionString)) {
+					// Make suggestion.
+					if (suspectSolutionString == null || weaponSolutionString == null) {
+						
+					}
+					// Make accusation.
+					else {
+						
+					}
+				}
+				// In other room. Needs to get out and try to reach solution room.
+				else {
+					
+					
+					//TODO igual a corridar mas ver cada porta da sala onde está
+				}
+			}
+			// In the corridor
+			else {
+				
+			}
+		}
+		// Room not known
+		else {
+			askDiceRoll();
+		}
 		
 	}
 
@@ -64,8 +102,68 @@ public class SmartBotPlayer extends BotPlayerAgent {
 
 	@Override
 	public void handleDiceRollResult(ACLMessage msg, int diceResult) {
-		// TODO Auto-generated method stub
+		Tile currentTile = gameState.board.getTileAtPosition(posOnBoard);
+		
+		// Room is known.
+		if (roomSolutionString != null) {
+			if (currentTile.isRoom()) {
+				// In solution room.
+				if (currentTile.getRoom().equals(roomSolutionString)) {
+					// Make suggestion.
+					if (suspectSolutionString == null || weaponSolutionString == null) {
+						
+					}
+					// Make accusation.
+					else {
+						
+					}
+				}
+				// In other room. Needs to get out and try to reach solution room.
+				else {
+					
+					
+					//TODO igual a corridar mas ver cada porta da sala onde está
+				}
+			}
+			// In the corridor
+			else {
+				int minDist = 9999;
+				for(Tile door : gameState.board.getRoomDoors(roomSolutionString)) {
+					ArrayList<Tile> tempPath;
+					if((tempPath = gameState.board.djs(posOnBoard, door.getCoordinates())).size() < minDist)
+						minPath = tempPath;
+				}
+			}
+		}
+		// Room not known
+		else {
+			if (currentTile.isRoom()) {
+				//TODO verificar se room is checked e mover ou fazer sugestao
+			}
+			// In the corridor
+			else { //go to the closest unchecked room
+				int minDist = 9999;
+				ArrayList<Tile> tempPath;
+				for(String room : playerNotebook.getNotCheckedRooms()) {
+					if((tempPath = getMinDistToRoom(room,posOnBoard,diceResult)).size() < minDist)
+						minPath = tempPath;
+				}
+			targetCoord = minPath.get(diceResult-1).getCoordinates();
+			}
+			
+			
+			
+		}
 		
 	}
-	
+	private ArrayList<Tile> getMinDistToRoom(String room, Coordinates source, int diceResult) {
+		int minDist = 9999;
+		ArrayList<Tile> bestPath = null;
+		for(Tile door : gameState.board.getRoomDoors(room)) {
+			ArrayList<Tile> tempPath;
+			if((tempPath = gameState.board.djs(source, door.getCoordinates())).size() < minDist)
+				bestPath = tempPath;
+		}
+		return bestPath;
+	}
 }
